bf3aabe9523d44adcd51e14b417bffebf8dfeb1b
Laibanoor12. <laibanoor4332@gmail.com>
2026-02-24 03:31:36 -0700

    my program

--- lesson 4.py
+++ lesson 4.py
@@ -4,3 +4,11 @@ num2 = 34
 add = num1 + num2 
 res = add/2
 print(num1)
+
+num1 =int input("enterfirst number to add")
+num2 = int input("enter sec number to add")
+add = num1 + num2
+res = add/2
+print(add)
+print(type(num))
+
