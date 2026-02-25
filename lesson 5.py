0c9b73535922047fe1e7a7a47a0ee99272ffba59
Laibanoor12. <laibanoor4332@gmail.com>
2026-02-25 03:13:37 -0700

    lesson 5

--- lesson 5.py
+++ lesson 5.py
@@ -1,5 +1,12 @@
 # Write a program that get two integer values from the user and multiply to output result
-num1 = int(input("enter first number to multiply: "))
-num2 = int(input("enter first number to multiply: "))
-result = num1*num2
-print("the result of two numbers multiplication is",result )
+# num1 = int(input("enter first number to multiply: "))
+# num2 = int(input("enter first number to multiply: "))
+# result = num1*num2
+# print("the result of two numbers multiplication is",result )
+print (3 == 6)
+print (5==7)
+print (5!=5)
+print(55==6)
+print(37==7)
+print(36==7)
+print(47!=6)
