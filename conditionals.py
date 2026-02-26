792b123307d9d9253346e2c73bcf84c7d7825027
Laibanoor12. <laibanoor4332@gmail.com>
2026-02-26 03:24:17 -0700

    input

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+value1 = 43
+val = 20
+print(value1<=val) 
+print(4==7)
+
+
+
+
+
+                                             
--- lesson 4.py
+++ lesson 4.py
@@ -1,14 +1,20 @@
 #ADD TWO VALUES 
-num1 = 4
-num2 = 34
-add = num1 + num2 
-res = add/2
-print(num1)
+# num1 = 4
+# num2 = 34
+# add = num1 + num2 
+# res = add/2
+# print(num1)
 
-num1 =int input("enter first number to add ")
-num2 = int input("enter sec number to add")
-add = num1 + num2
-res = add/2
-print(add)
-print(type(num))
+# num1 =int input("enter first number to add ")
+# num2 = int input("enter sec number to add")
+# add = num1 + num2
+# res = add/2
+# print(add)
+# print(type(num))
 
+num1 =int(input("enter first number to muliply: "))
+num2 =int(input("enter first number to multiply: "))
+result = num1*num2
+print("the result of two numbers multiplication is",result)
+
+          
