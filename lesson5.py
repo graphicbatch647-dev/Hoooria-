13d0a301cc845f688924eb956322475eaa7d7209
Laibanoor12. <laibanoor4332@gmail.com>
2026-02-28 03:37:59 -0700

    if else

--- lesson5.py
+++ lesson5.py
@@ -1,4 +1,14 @@
-num1 = int(input("enter my number"))
-num2 = int(input("enter my number"))
-result = num1 * num2
-print("result=",result)
+# # num1 = int(input("enter my number"))
+# # num2 = int(input("enter my number"))
+# # result = num1 * num2
+# # print("result=",result)
+
+# num1 = int(input("enter my number:"))
+# if num1 > 10 :
+# print ("num1 10 sy bara ha")
+
+marks = float(input("Enter a number"))
+if marks >= 5 :
+    print("Congratulations your's passed")
+else : 
+    print("Sorry you're failed,try again")
